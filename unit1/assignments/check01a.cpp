b28845c9707bbce7474b21d7b238fa15d3afe137
Joshua Pearson <mewurby@gmail.com>
2019-01-08 17:45:49 -0700

    updated name

---
+++ unit1/assignments/check01a.cpp
@@ -0,0 +1,57 @@
+/***********************************************************************
+* Program:
+*    Checkpoint 01, C++ BASICS          (e.g. Checkpoint 01a, review)  
+*    Brother Dudley, CS165
+* Author:
+*    Joshua Pearson
+* Summary: 
+*    Summaries are not necessary for checkpoint assignments.
+* ***********************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void display(string name, int age);
+string getName();
+int getAge();
+void greeting();
+/**********************************************************************
+ * Function: main
+ * Purpose: This is the entry point and driver for the program.
+ ***********************************************************************/
+int main()
+{
+   greeting();
+   display(getName(), getAge());
+
+
+   return 0;
+}
+
+string getName()
+{
+   string name;
+   cout << "Please enter your first name: ";
+   cin >> name;
+   return name;
+}
+
+int getAge()
+{
+   int age;
+   cout << "Please enter your age: ";
+   cin >> age;
+   return age;
+}
+
+void display(string name, int age)
+{
+   cout << "Hello " << name << ", you are "
+        << age << " years old.\n";
+}
+
+void greeting()
+{
+   cout << "Hello CS 165 World!\n";
+}
