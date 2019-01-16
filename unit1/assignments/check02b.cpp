a02d89f7a5ac185cfa594ccb11c0cb7562aecda0
Joshua Pearson <mewurby@gmail.com>
2019-01-15 18:25:12 -0700

    adding file for initial testBed testing

--- unit1/assignments/check02b.cpp
+++ unit1/assignments/check02b.cpp
@@ -12,23 +12,43 @@
 using namespace std;
 
 // TODO: Define your Complex struct here
-
+struct Complex
+{
+   double realPart;
+   double imaginaryPart;
+};
 
 // TODO: Add your prompt function here
-
+void prompt(Complex & x, Complex & y)
+{
+   cout << "Real: ";
+   cin >> x.realPart;
+   cin.ignore();
+   cout << "Imaginary: ";
+   cin >> x.imaginaryPart;
+   cout << "Real: ";
+   cin >> y.realPart;
+   cin.ignore();
+   cout << "Imaginary: ";
+   cin >> y.imaginaryPart;
+}
 
 // TODO: Add your display function here
-
+void display(Complex & sum)
+{
+   cout << "The sum is: " << sum.realPart << " + " << sum.imaginaryPart
+        << "i";
+}
 
 /**********************************************************************
  * Function: addComplex
  * Purpose: Adds two complex numbers together and returns the sum.
  ***********************************************************************/
-Complex addComplex(const Complex & x, const Complex & y)
+void addComplex(const Complex & x, const Complex & y, Complex & sum)
 {
    // TODO: Fill in the body of the add function
-
-
+   sum.realPart = x.realPart + y.realPart;
+   sum.imaginaryPart = x.imaginaryPart + y.imaginaryPart;
 }
 
 
@@ -39,16 +59,17 @@ Complex addComplex(const Complex & x, const Complex & y)
 int main()
 {
    // Declare two Complex objects, c1 and c2
-
+   Complex c1;
+   Complex c2;
 
    // Call your prompt function twice to fill in c1, and c2 
-
+   prompt(c1, c2);
 
    // Declare another Complex for the sum
-
+   Complex sum;
 
    // Call the addComplex function, putting the result in sum;
-
+   addComplex(c1, c2, sum);
    
    cout << "\nThe sum is: ";
    display(sum);
