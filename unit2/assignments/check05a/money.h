e8c70d4aaa3165602371a88f86484fae974f3148
Joshua Pearson <mewurby@gmail.com>
2019-02-07 01:33:44 -0700

    made the thing

--- unit2/assignments/check05a/money.h
+++ unit2/assignments/check05a/money.h
@@ -9,10 +9,25 @@ class Money
 private:
    int dollars;
    int cents;
-
+   void setCents(int cents)
+   {
+      this->cents = cents; 
+   }
+   void setDollars(int dollars)
+   {
+      this->dollars = dollars;
+   }
 public:
    void prompt();
    void display() const;
+   void getCents()
+   {
+      return cents;
+   }
+   void getDollars()
+   {
+      return dollars;
+   }
 };
 
 #endif
