ae827827fc058bd60d37585c909ebed16ff1bf08
Joshua Pearson <mewurby@gmail.com>
2019-02-11 14:38:54 -0700

    made stuff ready to test?

---
+++ unit2/projects/test-project/Test Project/Test Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test Project
+//
+//  Created by Joshua Pearson on 2/11/19.
+//  Copyright © 2019 Joshua Pearson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
