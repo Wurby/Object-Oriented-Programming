8d4a9c6f65e7f9aad39783a48226519295097506
Joshua Pearson <mewurby@gmail.com>
2019-02-12 01:06:30 -0700

    moved

---
+++ unit2/assignments/assign04/assign04.cpp
@@ -0,0 +1,47 @@
+/***************************************************************
+ * File: assign04.cpp
+ * Author: Joshua Pearson
+ * Purpose: Contains the main function to test the Product class.
+ ***************************************************************/
+
+#include <iostream>
+#include <string>
+
+
+#include "product.h"
+
+int main()
+{
+   Product product;
+
+   product.prompt();
+
+
+   std::cout << std::endl;
+   std::cout << "Choose from the following options:\n";
+   std::cout << "1 - Advertising profile\n";
+   std::cout << "2 - Inventory line item\n";
+   std::cout << "3 - Receipt\n";
+   std::cout << std::endl;
+   std::cout << "Enter the option that you would like displayed: ";
+
+   int choice;
+   std::cin >> choice;
+
+   std::cout << std::endl;
+
+   if (choice == 1)
+   {
+      product.displayAdvertising();
+   }
+   else if (choice == 2)
+   {
+      product.displayInventoryLine();
+   }
+   else
+   {
+      product.displayReceipt();
+   }
+
+   return 0;
+}
