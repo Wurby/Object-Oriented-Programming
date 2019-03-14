eb99778d9ab7b023f377ad2a28bd30f5d1b1eead
Joshua Pearson <mewurby@gmail.com>
2019-03-13 19:34:41 -0600

    finished check

--- unit4/assignments/check10a.cpp
+++ unit4/assignments/check10a.cpp
@@ -1,15 +1,16 @@
 /***********************************************************************
 * Program:
 *    Checkpoint 10a, Vectors
-*    Brother {Burton, Falin, Ercanbrack}, CS165
+*    Brother Dudley, CS165
 * Author:
-*    your name
+*    Joshua Pearson
 * Summary: 
 *    Summaries are not necessary for checkpoint assignments.
 ************************************************************************/
 
-
-
+#include <vector>
+#include <iostream>
+#include <string>
 
 /**********************************************************************
  * Function: main
@@ -17,10 +18,38 @@
  ***********************************************************************/
 int main()
 {
+   std::vector<int> ints;
+   std::vector<std::string> strings;
+   int enteredInt;
+   std::string enteredString;
 
+   do
+   {
+      std::cout << "Enter int: ";
+      std::cin >> enteredInt;
+      if (enteredInt != 0)
+         ints.push_back(enteredInt);
+   } while (enteredInt != 0);
 
+   std::cout << "Your list is: " << std::endl;
+   for (std::vector<int>::iterator it = ints.begin(); it != ints.end(); ++it)
+   {
+      std::cout << *it << std::endl;
+   }
+
+   do
+   {
+      std::cout << "Enter string: ";
+      std::cin >> enteredString;
+      if (enteredString != "quit")
+         strings.push_back(enteredString);
+   } while (enteredString != "quit");
+
+   std::cout << "Your list is: " << std::endl;
+   for (std::vector<std::string>::iterator it = strings.begin(); it != strings.end(); ++it)
+   {
+      std::cout << *it << std::endl;
+   }
 
    return 0;
 }
-
-
